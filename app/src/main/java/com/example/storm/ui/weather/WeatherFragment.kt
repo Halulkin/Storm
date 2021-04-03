69031cf75dcee84dd05b2e015742bfa180c80dce
“halulkin” <halulkin@gmail.com>
2021-04-03 08:50:16 +0200

    Implemented Dexter for checking permissions

--- app/src/main/java/com/example/storm/ui/weather/WeatherFragment.kt
+++ app/src/main/java/com/example/storm/ui/weather/WeatherFragment.kt
@@ -1,14 +1,31 @@
 package com.example.storm.ui.weather
 
+import android.Manifest
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.provider.Settings
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
+import com.example.storm.R
 import com.example.storm.databinding.FragmentWeatherBinding
+import com.google.android.gms.common.api.ResolvableApiException
+import com.google.android.gms.location.LocationRequest
+import com.google.android.gms.location.LocationServices
+import com.google.android.gms.location.LocationSettingsRequest
+import com.google.android.material.snackbar.Snackbar
+import com.karumi.dexter.Dexter
+import com.karumi.dexter.PermissionToken
+import com.karumi.dexter.listener.PermissionDeniedResponse
+import com.karumi.dexter.listener.PermissionGrantedResponse
+import com.karumi.dexter.listener.PermissionRequest
+import com.karumi.dexter.listener.single.PermissionListener
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class WeatherFragment : Fragment() {
@@ -26,13 +43,11 @@ class WeatherFragment : Fragment() {
         _binding = FragmentWeatherBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        weatherViewModel.getLastLocation()
+
+        getGpsLocation()
 
 
         val textView: TextView = binding.textDashboard
-//        weatherViewModel.text.observe(viewLifecycleOwner, Observer {
-//            textView.text = it
-//        })
 
         weatherViewModel.location.observe(viewLifecycleOwner, {
             Log.e(
@@ -42,10 +57,83 @@ class WeatherFragment : Fragment() {
             textView.text = it?.latitude.toString() + ", " + it?.longitude.toString()
         })
 
-//        weatherViewModel.location.observe(viewLifecycleOwner, Observer {
-//            textView.text = it.toString()
-//        })
         return root
+    }
+
+    private fun getGpsLocation() {
+        Log.e("TAG", "getGpsLocation")
+
+        Dexter.withContext(requireActivity())
+            .withPermission(Manifest.permission.ACCESS_FINE_LOCATION)
+            .withListener(object : PermissionListener {
+                override fun onPermissionGranted(response: PermissionGrantedResponse?) {
+                    val locationReq = LocationRequest.create().apply {
+                        priority = LocationRequest.PRIORITY_HIGH_ACCURACY
+                    }
+                    val builder = LocationSettingsRequest.Builder().apply {
+                        addLocationRequest(locationReq)
+                    }
+                    val result = LocationServices.getSettingsClient(requireActivity())
+                        .checkLocationSettings(
+                            builder.build()
+                        )
+                    result.addOnSuccessListener {
+//                        showWeather()
+                        weatherViewModel.getLastLocation()
+                        Toast.makeText(context, "Ready to show weather!", Toast.LENGTH_SHORT).show()
+                        Log.e("TAG", "addOnSuccessListener")
+
+                    }
+                    // when permission.ACCESS_FINE_LOCATION granted, but location is disabled
+                    // show location dialog
+                    result.addOnFailureListener { exception ->
+                        if (exception is ResolvableApiException) {
+                            Log.e("TAG", "addOnFailureListener")
+                            exception.startResolutionForResult(requireActivity(), 321)
+                        }
+                    }
+                }
+
+                override fun onPermissionDenied(response: PermissionDeniedResponse) {
+                    if (response.isPermanentlyDenied) {
+                        // show snackBar allowing app setting navigation
+                        Snackbar
+                            .make(
+                                binding.layout,
+                                getString(R.string.change_location_permission),
+                                Snackbar.LENGTH_LONG
+                            )
+                            .setAction(getString(R.string.ok)) {
+                                val intent =
+                                    Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS)
+                                val uri: Uri = Uri.fromParts(
+                                    "package",
+                                    requireActivity().packageName,
+                                    null
+                                )
+                                intent.data = uri
+                                startActivity(intent)
+                            }
+                            .show()
+                    } else {
+                        // show snackBar with rationale
+                        Snackbar
+                            .make(
+                                binding.layout,
+                                getString(R.string.permission_rejection_text),
+                                Snackbar.LENGTH_LONG
+                            )
+                            .show()
+                    }
+                }
+
+                override fun onPermissionRationaleShouldBeShown(
+                    request: PermissionRequest?,
+                    token: PermissionToken?
+                ) {
+                    token?.continuePermissionRequest()
+                }
+            }).check()
     }
 
     override fun onDestroyView() {
